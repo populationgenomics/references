fed4ac9c2deb9d0e58f7921c470ca0cd3daf284b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-11-25 00:34:00 +1100

    Fix changed

--- .github/workflows/prep_matrix.py
+++ .github/workflows/prep_matrix.py
@@ -18,11 +18,13 @@ transfers = {}
 for source in NEW_SOURCES:
     old_sources_d = {s.name: s for s in OLD_SOURCES}
     is_changed = (
-        source.name not in old_sources_d or source != old_sources_d[source.name]
+        source.name not in old_sources_d
+        or source.src != old_sources_d[source.name].src
+        or source.dst != old_sources_d[source.name].dst
     )
     dst_path = join(REFERENCES_PREFIX, source.dst)
     if is_changed and source.src:
-        print(f'{source.name} is changed, will transfer', file=sys.stderr)
+        print(f'{source.name} has changed, will transfer', file=sys.stderr)
         transfers[source.name] = {'src': source.src, 'dst': dst_path}
     else:
         print(f'{source.name} has not changed since previous revision', file=sys.stderr)
