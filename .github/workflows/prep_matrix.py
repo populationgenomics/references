e67fa42bd82c8b58967089c982b49abe43bef1ab
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-11-25 00:40:08 +1100

    Check dst exists

--- .github/workflows/prep_matrix.py
+++ .github/workflows/prep_matrix.py
@@ -5,6 +5,7 @@ Prepare test matrix (to transfer references in parallel)
 import os
 import sys
 from os.path import join
+from cloudpathlib import AnyPath
 from references import SOURCES as NEW_SOURCES
 
 try:
@@ -17,12 +18,13 @@ REFERENCES_PREFIX = os.environ['REFERENCES_PREFIX']
 transfers = {}
 for source in NEW_SOURCES:
     old_sources_d = {s.name: s for s in OLD_SOURCES}
+    dst_path = join(REFERENCES_PREFIX, source.dst)
     is_changed = (
         source.name not in old_sources_d
         or source.src != old_sources_d[source.name].src
         or source.dst != old_sources_d[source.name].dst
+        or not AnyPath(dst_path).exists()
     )
-    dst_path = join(REFERENCES_PREFIX, source.dst)
     if is_changed and source.src:
         print(f'{source.name} has changed, will transfer', file=sys.stderr)
         transfers[source.name] = {'src': source.src, 'dst': dst_path}
