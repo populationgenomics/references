697bae4a4d20fa98a338b9e1c43bbff3a8218d7f
mgeaghan <michael.p.geaghan@gmail.com>
2023-12-20 11:36:36 +1100

    save gtf and fa files

---
+++ star/prod_sync_script.fa_gtf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# check that the source location exists
+echo "Checking gs://cpg-common-main-tmp/references/star/hg38"
+if gcloud storage ls "gs://cpg-common-main-tmp/references/star/hg38" > /dev/null; then
+    echo "Source location exists, continuing"
+else
+    echo "Source location vacant, exiting"
+    exit 1
+fi
+
+# check that the destination directory doesn't already exist
+echo "Checking gs://cpg-common-main/references/star/hg38"
+if gcloud storage ls "gs://cpg-common-main/references/star/hg38" > /dev/null; then
+    echo "Target location already exists, will not replace"
+    exit 1
+else
+    echo "Target location vacant, copying"
+    gsutil rsync -r -m "gs://cpg-common-main-tmp/references/star/hg38" "gs://cpg-common-main/references/star/hg38"
+fi
