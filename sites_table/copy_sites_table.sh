b986bc423f05794046c9a896a3b0c936f1cee197
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2024-01-10 10:16:52 +1100

    Add recursive flag, as well as update README (#43)

--- sites_table/copy_sites_table.sh
+++ sites_table/copy_sites_table.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-gsutil -m cp gs://cpg-hgdp-1kg-main/sites_table/v1-0/pruned_variants.ht gs://cpg-common-main/references/ancestry/
+gsutil -m cp -r gs://cpg-hgdp-1kg-main/sites_table/v1-0/pruned_variants.ht gs://cpg-common-main/references/ancestry/
