fee115344b7b3e7a3c75a69283320a32e1e0de9d
MattWellie <mattwellie@gmail.com>
2025-02-28 07:49:11 +1000

    also create a compressed representation

--- reference_generating_scripts/alphamissense_manager.sh
+++ reference_generating_scripts/alphamissense_manager.sh
@@ -8,17 +8,23 @@ WRITE_OUTPUT_TO="gs://cpg-common-main/references/alphamissense"
 AM_ZENODO="https://zenodo.org/records/8208688/files/AlphaMissense_hg38.tsv.gz"
 
 # Download the raw data
-wget -q -O AlphaMissense_hg38.tsv.gz $AM_ZENODO
+wget -q -O alphamissense_38.tsv.gz $AM_ZENODO
 
 # copy it up to GCP
-TSV_DESTINATION="${WRITE_OUTPUT_TO}/AlphaMissense_hg38.tsv.gz"
-gcloud storage cp --do-not-decompress AlphaMissense_hg38.tsv.gz $TSV_DESTINATION
+TSV_DESTINATION="${WRITE_OUTPUT_TO}/alphamissense_38.tsv.gz"
+gcloud storage cp --do-not-decompress alphamissense_38.tsv.gz $TSV_DESTINATION
 
 # convert the TSV to a HT
 python3 reference_generating_scripts/alphamissense_formatting.py \
-    --am_tsv AlphaMissense_hg38.tsv.gz \
-    --ht_out output.ht
+    --am_tsv alphamissense_38.tsv.gz \
+    --ht_out alphamissense_38.ht
 
 # copy that up to GCP
-TABLE_DESTINATION="${WRITE_OUTPUT_TO}/AlphaMissense_hg38.ht"
-gcloud --quiet storage cp -r output.ht $TABLE_DESTINATION
+TABLE_DESTINATION="${WRITE_OUTPUT_TO}/alphamissense_38.ht"
+gcloud --quiet storage cp -r alphamissense_38.ht $TABLE_DESTINATION
+
+# compress the output, write as a single file (easier to localise)
+tar -czf alphamissense_38.ht.tar.gz alphamissense_38.ht
+
+TAR_DESTINATION="${WRITE_OUTPUT_TO}/alphamissense_38.ht.tar.gz"
+gcloud --quiet storage cp alphamissense_38.ht $TAR_DESTINATION
