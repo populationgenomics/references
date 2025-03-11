ed1f940f666145d2bb402cf877027b063f843f9d
MattWellie <mattwellie@gmail.com>
2025-03-11 11:27:15 +1000

    flip-flop for test outputs

--- reference_generating_scripts/generate_bed_from_ensembl_manager.sh
+++ reference_generating_scripts/generate_bed_from_ensembl_manager.sh
@@ -2,7 +2,6 @@
 
 # This script is used to manage the generation of a BED file from the ensembl GFF3
 
-# destination for the output
 
 # first identify the location of the ensembl GFF3 based on its version
 ENSEMBL_VERSION=${1:-"113"}
@@ -10,6 +9,9 @@ GFF3_URL="https://ftp.ensembl.org/pub/release-${ENSEMBL_VERSION}/gff3/homo_sapie
 LOCAL_OUTPUT_GFF3="GRCh38.gff3.gz"
 LOCAL_OUTPUT_BED="GRCh38.bed"
 MERGED_OUTPUT_BED="merged_GRCh38.bed"
+
+# destination for the output
+MAIN_OR_TEST=${2:-"main"}
 
 # get that
 wget "${GFF3_URL}" -O "${LOCAL_OUTPUT_GFF3}"
@@ -27,7 +29,7 @@ sort -k1,1V -k2,2n unmerged.bed > "${LOCAL_OUTPUT_BED}"
 sort -k1,1V -k2,2n merged.bed > "${MERGED_OUTPUT_BED}"
 
 # copy all the content to GCP - keep the same file names, copy in bulk
-WRITE_OUTPUT_TO="gs://cpg-common-main/references/ensembl_${ENSEMBL_VERSION}"
+WRITE_OUTPUT_TO="gs://cpg-common-${MAIN_OR_TEST}/references/ensembl_${ENSEMBL_VERSION}"
 #GFF3_DESTINATION="${WRITE_OUTPUT_TO}/${LOCAL_OUTPUT_GFF3}"
 #UNMERGED_BED_DESTINATION="${WRITE_OUTPUT_TO}/${LOCAL_OUTPUT_BED}"
 #MERGED_BED_DESTINATION="${WRITE_OUTPUT_TO}/${MERGED_OUTPUT_BED}"
